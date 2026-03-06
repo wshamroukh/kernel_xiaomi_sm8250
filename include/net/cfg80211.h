2e48ca47029bf384cc5c29bb5a0ee64861d02e0c
Veerendranath Jakkam <quic_vjakkam@quicinc.com>
2026-03-06 22:56:56 +0100

    nl80211: Advertise maximum number of AKM suites supported
    Increase akm_suites array size in struct cfg80211_crypto_settings to
    CFG80211_MAX_NUM_AKM_SUITES (16) and advertise the capability to
    userspace. This allows userspace to send more than two AKMs to driver
    in netlink commands such as NL80211_CMD_CONNECT.
    
    On Android 16 QPR2, wificond queries this attribute in GET_WIPHY
    response and logs "Failed to get NL80211_ATTR_MAX_NUM_AKM_SUITES"
    when missing, falling back to max_num_akms=1.
    
    - Define NL80211_ATTR_MAX_NUM_AKM_SUITES with explicit numeric value
      matching upstream (315).
    - Define CFG80211_MAX_NUM_AKM_SUITES (16) as the upper bound for
      the AKM suites array in cfg80211_crypto_settings.
    - Add max_num_akm_suites field to struct wiphy.
    - Initialize default to NL80211_MAX_NR_AKM_SUITES (2) in
      wiphy_register(), validate driver-provided values.
    - Report the attribute in nl80211_send_wiphy() GET_WIPHY response.
    - Use wiphy.max_num_akm_suites for validation in
      nl80211_crypto_settings() instead of the hardcoded limit.
    - Set wiphy->max_num_akm_suites in qcacld-3.0 driver to its
      internal MAX_NUM_AKM_SUITES (16).
    
    Change-Id: I531b366158b1bb4176900c7e7eebaa52cece2897
    Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>
    Signed-off-by: Quince <quinceroms@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -860,13 +860,15 @@ struct survey_info {
  * @wep_tx_key: key index (0..3) of the default TX static WEP key
  * @psk: PSK (for devices supporting 4-way-handshake offload)
  */
+#define CFG80211_MAX_NUM_AKM_SUITES	16
+
 struct cfg80211_crypto_settings {
 	u32 wpa_versions;
 	u32 cipher_group;
 	int n_ciphers_pairwise;
 	u32 ciphers_pairwise[NL80211_MAX_NR_CIPHER_SUITES];
 	int n_akm_suites;
-	u32 akm_suites[NL80211_MAX_NR_AKM_SUITES];
+	u32 akm_suites[CFG80211_MAX_NUM_AKM_SUITES];
 	bool control_port;
 	__be16 control_port_ethertype;
 	bool control_port_no_encrypt;
@@ -4254,6 +4256,7 @@ struct wiphy {
 
 	const struct wiphy_iftype_akm_suites *iftype_akm_suites;
 	unsigned int num_iftype_akm_suites;
+	u16 max_num_akm_suites;
 
 	u8 retry_short;
 	u8 retry_long;
